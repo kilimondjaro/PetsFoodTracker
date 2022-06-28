8b5f8aedff447d7d321ded6449ffd50498ea396c
Kirill Babich <kirill.babich@mail.ru>
2022-06-28 09:21:34 +0200

    Add theme

---
+++ src/shared/ui/theme/index.ts
@@ -0,0 +1,36 @@
+import { createTheme } from '@shopify/restyle';
+
+const palette = {
+  salmonGradient1: 'hsla(12, 84%, 71%, 1)',
+  salmonGradient2: 'hsla(12, 84%, 71%, 1)',
+  salmon: 'hsla(360, 97%, 73%, 1)',
+  blue: 'hsla(231, 32%, 30%, 1)',
+  grey: 'hsla(231, 9%, 56%, 1)',
+  pinkyWhite: 'hsla(12, 83%, 98%, 0.37)',
+  transparentWhite: 'hsla(0, 0%, 100%, 0.7)',
+};
+
+const theme = createTheme({
+  colors: {
+    mainBackground: palette.pinkyWhite,
+    transparentBackground: palette.transparentWhite,
+
+    primaryGradient1: palette.salmonGradient1,
+    primaryGradient2: palette.salmonGradient2,
+
+    textPrimary: palette.blue,
+    textSecondary: palette.grey,
+  },
+  spacing: {
+    xs: 8,
+    s: 12,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+  breakpoints: {
+    phone: 0,
+  },
+});
+
+export default theme;
---
+++ src/shared/ui/theme/types.ts
@@ -0,0 +1,3 @@
+import type theme from './index';
+
+export type Theme = typeof theme;
